e4673fde9704f895d64dd9e810e49fae2b2817ab
r0lfs <rdwoolen@gmail.com>
2017-11-09 14:53:48 -0700

    made primary controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/show'
+
+  get 'comments/edit'
+
+  get 'comments/update'
+
+  get 'comments/destroy'
+
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
 end
