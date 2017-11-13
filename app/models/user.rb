5169a5c94ff6bd598ab2181f036865546545c349
r0lfs <rdwoolen@gmail.com>
2017-11-12 23:30:22 -0700

    added some styling, made it so no duplicate emails or usernames can exist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	validates :email, uniqueness: true
+	validates :dis_name, uniqueness: { case_sensitive: false }
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
 end
